52d0f8849a46dc23c775e23c67020c5913d390ce
mofde <mofde@udel.edu>
2023-05-10 19:11:47 -0400

    Final Commit?

--- project2.py
+++ project2.py
@@ -2,45 +2,38 @@ from collections import OrderedDict, deque
 import copy
 from math import sqrt
 from flask import Flask, render_template, request
-from ast import literal_eval
 import sudoku_constraints
 app = Flask(__name__)
 
-def revise(csp, variableOneName, variableTwoName):
+def revise(csp, variableOneName, variableTwoName, assignment):
+    numChanges = 0
     newDomain = []
     for i in range(len(csp.get("variables").get(variableOneName))):
         for j in range(len(csp.get("variables").get(variableTwoName))):
-            if csp.get("variables").get(variableOneName)[i] != csp.get("variables").get(variableTwoName)[j]:
+            if csp.get("variables").get(variableOneName)[i] != csp.get("variables").get(variableTwoName)[j] and csp.get("variables").get(variableOneName)[i] not in newDomain:
                 newDomain.append(csp.get("variables").get(variableOneName)[i])
-                break
     numChanges = len(csp.get("variables").get(variableOneName)) - len(newDomain)
-    if len(csp.get("variables")[variableOneName]) > 1:
-        csp.get("variables")[variableOneName] = newDomain
+    newDomain.sort()
+    csp.get("variables")[variableOneName] = newDomain
+    if len(csp.get("variables")[variableOneName]) == 1:
+        assignment[variableOneName] = csp.get("variables")[variableOneName][0]
     return numChanges != 0
 
-def ac3(csp):
+def ac3(csp, assignment):
     queue = deque()
     constraintsList = list(csp.get("constraints").keys())
     for constraint in constraintsList:
         queue.appendleft(constraint)
-        queue.appendleft((constraint[1], constraint[0]))
     while len(queue) != 0:
         constraint = queue.pop()
-        if revise(csp, constraint[0], constraint[1]):
+        if revise(csp, constraint[0], constraint[1], assignment):
             if len(csp.get("variables").get(constraint[0])) == 0:
                 return False
-            for variable in csp.get("constraints"):
-                #neighbors in a row
-                if variable[1] == constraint[0] and variable[0] != constraint[1]:
-                    queue.appendleft(variable)
-                #neighbors in a column
-                elif variable[0] == constraint[0] and variable[1] != constraint[1]:
-                    queue.appendleft((variable[1], constraint[0]))
     return True
 
 def minimumRemainingValues(csp, assignments):
     for variable in assignments.keys():
-        csp["variables"][variable] = assignments.get(variable)
+        csp["variables"][variable] = [assignments.get(variable)]
     minLength = int(sqrt(len(csp["variables"].keys()))) + 1
     finalVariable = ""
     for variable in csp["variables"].keys():
@@ -54,21 +47,22 @@ def backtrack(csp, assignment):
     for variable in csp.get("variables").keys():
         if len(csp.get("variables").get(variable)) == 1:
             numFilledTiles += 1
-    #if len(assignment) == len(csp.get("variables").keys()):
-        #return assignment
     if numFilledTiles == len(csp["variables"].keys()):
         return assignment
     var = minimumRemainingValues(csp, assignment)
+    oldDomain = copy.deepcopy(csp["variables"][var])
     for value in csp["variables"][var]:
-        oldDomains = copy.deepcopy(csp["variables"])
-        csp["variables"][var] = [value]
-        if ac3(csp):
+        cspCopy = copy.deepcopy(csp)
+        cspCopy["variables"][var] = [value]
+        if ac3(cspCopy, assignment):
             assignment[var] = value
+            csp["variables"][var] = [value]
             result = backtrack(csp, assignment)
             if result != None:
                 return result
+            csp["variables"][var] = oldDomain
+    if var in assignment.keys():
         del assignment[var]
-        csp["variables"] = oldDomains
     return None
 
 def backtrackingSearch(csp):
@@ -76,7 +70,7 @@ def backtrackingSearch(csp):
 
 @app.route('/')
 def createWebsite():
-    return render_template('form.html')
+    return render_template('inputSudoku.html')
 
 @app.route('/solve')
 def saveSudoku():
@@ -94,13 +88,14 @@ def saveSudoku():
                 csp["variables"][var] = list(range(1, 10))
     boards = [board]
     solution = backtrackingSearch(csp)
-    for var in solution:
-        board = copy.deepcopy(board)
-        row = int(var[1])
-        col = int(var[2])
-        board[row-1][col-1] = solution[var]
-        boards.append(board)
-    return render_template('savedSudoku.html', boards=boards)
+    if solution != None:
+        for var in solution:
+            board = copy.deepcopy(board)
+            row = int(var[1])
+            col = int(var[2])
+            board[row-1][col-1] = solution[var]
+            boards.append(board)
+    return render_template('sudokuSolution.html', boards=boards)
 
 if __name__ == "__main__":
     app.debug = True
